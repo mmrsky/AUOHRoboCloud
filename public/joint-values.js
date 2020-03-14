84f2f68310904bce4e97f04f1f4a402ec4c0ccdd
mika <mika.morsky@gmail.com>
2020-03-14 09:48:29 +0200

    more stuff

--- public/joint-values.js
+++ public/joint-values.js
@@ -1,9 +1,10 @@
-const mqtt_client = mqtt.connect('wss://mqtt-worker.herokuapp.com');
+const mqtt_client = mqtt.connect('wss://auoh-mqtt-broker.herokuapp.com/');
+//const mqtt_client = mqtt.connect('wss://mqtt-worker.herokuapp.com/');
 mqtt_client.on('connect', () => {
-console.log('connected to mqtt broker');
-mqtt_client.subscribe('joints')
+    console.log('connected to mqtt broker');
+    mqtt_client.subscribe('joints')
 });
 mqtt_client.on('message', (topic, message) => {
-const joint_data = JSON.parse(message)
-document.body.innerHTML = JSON.stringify(joint_data);
+    const joint_data = JSON.parse(message)
+    document.body.innerHTML = JSON.stringify(joint_data);
 });
